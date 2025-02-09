1d6cbffdeca7bd7f5e0351c69cf025ba0ab9a095
Alexis VACHET <alexis.vachet@outlook.fr>
2025-02-10 00:04:03 +0100

    install eslint

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-red-500">Hello world</h1>
+  <div class="">
+    <h1 class="text-white p-3 rounded-2xl w-fit bg-blue-500 ">Hello world</h1>
   </div>
 </template>
