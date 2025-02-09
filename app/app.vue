ef793ee56616608f02ae67c0009933f75e9d7153
Alexis VACHET <alexis.vachet@outlook.fr>
2025-02-09 23:46:07 +0100

    install tailwind

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-
+    <h1 class="text-red-500">Hello world</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
+
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
