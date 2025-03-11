709fde615b04e2fae930ef6678d7283007b2519e
Alexis VACHET <alexis.vachet@outlook.fr>
2025-03-11 16:38:39 +0100

    feat: update profile picture uploader with improved UI, reload on upload, and refactor database connection to use environment variables

--- app/components/ProfilePictureUploader.vue
+++ app/components/ProfilePictureUploader.vue
@@ -46,10 +46,9 @@ const cropImage = async () => {
         const newProfilePicture = await uploadProfilePicture(file, isUpdate);
 
         if (newProfilePicture) {
-            profilePicture.value = newProfilePicture;
-            if (user.value) {
-                user.value.profilePicture = newProfilePicture;
-            }
+            setTimeout(() => {
+                window.location.reload();
+            }, 500);
         }
     }, "image/jpeg");
 };
@@ -58,20 +57,27 @@ const cropImage = async () => {
 
 
 <template>
-    <div class="flex flex-col items-center space-y-4">
+    <div class="flex flex-col items-center">
         <!-- Placeholder pour sélectionner une nouvelle image -->
-        <label for="fileInput"
-            class="cursor-pointer relative w-32 h-32 flex items-center justify-center rounded-full border-2 border-gray-400 overflow-hidden">
-            <img v-if="profilePicture" :src="profilePicture" class="w-full h-full object-cover" />
-            <Icon v-else name="lucide:circle-user-round" class="text-gray-400 text-9xl" />
-            <input id="fileInput" type="file" accept="image/*" @change="onFileChange" class="hidden" ref="fileInput" />
-        </label>
+        <div class="group relative">
+            <label for="fileInput"
+                class="cursor-pointer relative w-32 h-32 flex items-center justify-center rounded-full border-2 border-neutral-900 overflow-hidden mt-10">
+                <img v-if="profilePicture" :src="profilePicture" class="w-full h-full object-cover" />
+                <Icon v-else name="lucide:circle-user-round" class="text-gray-400 text-9xl" />
+                <input id="fileInput" type="file" accept="image/*" @change="onFileChange" class="hidden"
+                    ref="fileInput" />
+            </label>
+            <Icon name="icon-park-twotone:add-one"
+                class="absolute top-[80%] right-2 z-10 text-2xl opacity-50 group-hover:opacity-100 transition-all duration-200 ease-in-out" />
+        </div>
+
+
 
         <!-- Recadrage de l'image -->
-        <div v-if="image" class="cropper-container">
+        <div v-if="image" class="cropper-container flex flex-col items-center">
             <Cropper ref="cropper" :src="image" :stencil-props="{ aspectRatio: 1 }" class="cropper" />
-            <button @click="cropImage" class="mt-4 bg-green-500 text-white px-4 py-2 rounded-lg cursor-pointer">
-                Recadrer & Enregistrer
+            <button @click="cropImage" class="mt-4 bg-orange-500 text-white px-4 py-2 rounded-lg cursor-pointer">
+                Valider
             </button>
         </div>
     </div>
--- server/db.ts
+++ server/db.ts
@@ -1,14 +1,12 @@
-// server/db.ts
 import pkg from "pg";
 const { Pool } = pkg;
-const config = useRuntimeConfig();
 
 const pool = new Pool({
-  host: config.dbHost,
-  port: parseInt(config.dbPort),
-  user: config.dbUser,
-  password: config.dbPassword,
-  database: config.dbName,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
