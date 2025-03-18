3e434a48aed1735d55a0fa3722923f3fd67faf0a
Alexis VACHET <alexis.vachet@outlook.fr>
2025-03-19 00:03:20 +0100

    feat: add LoadingState and EmptyBlocState components; refactor index.vue template

---
+++ app/components/shared/LoadingState.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex h-screen items-center justify-center">
+    <Icon
+      name="svg-spinners:blocks-shuffle-3"
+      class="text-4xl text-orange-500"
+    />
+  </div>
+</template>
